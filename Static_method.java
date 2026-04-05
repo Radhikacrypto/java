a049783e7df5563f076d53bd5833c29ec28db8e5
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2026-04-05 08:03:36 +0530

    STatic method

--- Static_method.java
+++ Static_method.java
@@ -1,8 +1,18 @@
-Mobile
+class phone {
+    int year; 
+    String name; 
+    int price; 
+    static String manufacturer_country;
+    static String material_location;
+
+    public void show(){
+        System.out.printf(name +  ": " + year + ": " + price + " : ");
+    }
+}
 
 
 class Static_method {
     public static void main(String ar[]){
-
+        
     }
 }
