4feafea5f8790b331a9722a408a703a6fbd68978
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2026-04-10 00:51:30 +0530

    encapsulatioonin java

--- Encapsulation_method.java
+++ Encapsulation_method.java
@@ -1,10 +1,23 @@
 class Student{
     private int age;
-    
+    private String roll_no;
+    String name;
+
+    public  void show(int a, String b){
+        age=a;
+        roll_no= b;
+        System.out.printf(" the age is: "+age);
+        System.out.printf(" \n Roll number is :"+ roll_no);
+
+    }
 }
 
 class Encapsulated_method{
     public static void main(String ar[]){
 
+        Student s1= new Student();
+        // s1.age = something not allowed 
+         
+m
     }
 }
