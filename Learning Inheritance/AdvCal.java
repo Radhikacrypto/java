bd52c1e6a172ac242968944ceb42dedc65df7363
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2026-04-10 01:25:32 +0530

    instance varible inheritance

---
+++ Learning Inheritance/AdvCal.java
@@ -0,0 +1,21 @@
+public class AdvCal extends Calc {
+    // int a;
+    // int b;
+
+    // public void setValues(int a, int b){
+    //     this.a= a;
+    //     this.b= b;
+    // }
+
+
+    // still if we want we can taake following 
+    // public void setValues(int a, int b){
+    //     setVal(a, b);
+    // }
+
+    public int multiply(){
+        int result= val1*val2;//note here it is val 1 and val 2 
+        return result;
+    }
+
+}
--- Learning Inheritance/Demo.java
+++ Learning Inheritance/Demo.java
@@ -1,6 +1,9 @@
 public class Demo
 {
     public static void main(String ar[]){
-        
+        AdvCal Calc1= new AdvCal();
+        Calc1.setVal(30, 20);
+        int result= Calc1.add();
+        System.out.println(result);
     }
 }
